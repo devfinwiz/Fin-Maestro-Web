6d033307b676b5e9f8a1f72fa575c25355380575
Dev V Juneja <78873223+devfinwiz@users.noreply.github.com>
2023-02-18 22:22:16 +0530

    fetches nse indices pe history

---
+++ Prerequisites/nse_indices_pe_history.py
@@ -0,0 +1,21 @@
+import datetime
+from Prerequisites.constants import NSE_INDICES
+from nsepy import get_index_pe_history
+import concurrent.futures
+
+#---------------------------------------------------------------------------
+#Generates csv files for all NSE indices with their PE,P/B
+
+def dataset_generator(indice):  
+    history_filename="nse_indices_pe_dataset\{}.csv".format(indice)  
+    f=open(history_filename,'w',newline="")
+
+    indice_pe = get_index_pe_history(symbol="{}".format(indice),
+                                    start=datetime.datetime(2022,1,1),
+                                    end=datetime.datetime(2023,1,10))
+    f.write(indice_pe.to_csv())
+    f.close()
+
+if __name__=='main':
+     with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(dataset_generator,NSE_INDICES)
