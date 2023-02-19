5e324223c31b8b05f8222ac8f306e2629db9e3ad
DEV_FINWIZ <devjuneja43@gmail.com>
2023-02-19 14:35:16 +0530

    nse_indices_pe_history.py output dataset

--- Prerequisites/nse_indices_pe_history.py
+++ Prerequisites/nse_indices_pe_history.py
@@ -1,5 +1,5 @@
 import datetime
-from Prerequisites.constants import NSE_INDICES
+from constants import NSE_INDICES
 from nsepy import get_index_pe_history
 import concurrent.futures
 
@@ -7,12 +7,12 @@ import concurrent.futures
 #Generates csv files for all NSE indices with their PE,P/B
 
 def dataset_generator(indice):  
-    history_filename="nse_indices_pe_dataset\{}.csv".format(indice)  
+    history_filename="Dataset\Resultant Dataset\\nse_indices_pe_dataset\{}.csv".format(indice)  
     f=open(history_filename,'w',newline="")
 
     indice_pe = get_index_pe_history(symbol="{}".format(indice),
                                     start=datetime.datetime(2022,1,1),
-                                    end=datetime.datetime(2023,1,10))
+                                    end=datetime.datetime(2023,2,17))
     f.write(indice_pe.to_csv())
     f.close()
 
