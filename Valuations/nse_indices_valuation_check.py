135db7b4bfa12281d53013754ccef01b46589631
Dev V Juneja <78873223+devfinwiz@users.noreply.github.com>
2023-02-21 12:15:33 +0530

    differentiating logic for exceptional indices

--- Valuations/nse_indices_valuation_check.py
+++ Valuations/nse_indices_valuation_check.py
@@ -44,20 +44,30 @@ def indices_flag_allocator():
     indices_flag=dict()
     indices_median_pe=dict()
     indices_latest_pe=dict()
-    
+    exceptions=['NIFTY FMCG','NIFTY IT']
+
     for indice in NSE_INDICES:
-        df=pd.read_csv("Dataset\\Resultant Dataset\\nse_indices_pe_dataset\\{}.csv".format(indice))
+        df=pd.read_csv("Auto generated Dataset\\{}.csv".format(indice))
         indices_median_pe[indice]=round(df['P/E'].median(),2)
         indices_latest_pe[indice]=df['P/E'].iloc[-1]
     
     for indice,value in indices_median_pe.items():
-        if((value + (25*value)/100)<indices_latest_pe[indice]):
+        if(indice in exceptions and (value + (75*value)/100)<indices_latest_pe[indice]):
             indices_flag[indice]="RED"
-        elif((value + (5*value)/100)<indices_latest_pe[indice]):
+        elif(indice in exceptions and (value + (20*value)/100)<indices_latest_pe[indice]):
             indices_flag[indice]="ORANGE"
-        else:
+        elif(indice in exceptions and (value + (20*value)/100)>indices_latest_pe[indice]):
             indices_flag[indice]="GREEN"
 
+        if((value + (30*value)/100)<indices_latest_pe[indice] and indice not in exceptions):
+            indices_flag[indice]="RED"
+        elif((value + (6*value)/100)<indices_latest_pe[indice] and indice not in exceptions):
+            indices_flag[indice]="ORANGE"
+        else:
+            if(indice not in exceptions):
+                indices_flag[indice]="GREEN"
+
+    print(indices_median_pe)
     return indices_flag
 
 #--------------------------------------------------------------------------------------------------------
