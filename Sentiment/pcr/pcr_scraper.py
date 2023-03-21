821d2853494a3a07980d964280ccc52b4b1ec8c4
Dev V Juneja <78873223+devfinwiz@users.noreply.github.com>
2023-03-21 15:45:31 +0530

    pcr scraper for stocks added

--- Sentiment/pcr/pcr_scraper.py
+++ Sentiment/pcr/pcr_scraper.py
@@ -7,6 +7,7 @@ from cachetools import cached, TTLCache
 #possible symbol values: NIFTY/BANKNIFTY
 
 cache = TTLCache(maxsize=5, ttl=86400)
+cache2= TTLCache(maxsize=100, ttl=86400)
 
 @cached(cache)
 def pcr_scraper(symbol):
@@ -25,9 +26,31 @@ def pcr_scraper(symbol):
     pcr= totPE/totCE
     return round(pcr,3)
 
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+#returns pcr value for passed symbol
+#possible symbol values: NIFTY Constituents (Eg. ADANIENT, TATAMOTORS)
+
+@cached(cache2)
+def pcr_stocks_scraper(symbol):
+    url = 'https://www.nseindia.com/api/option-chain-equities?symbol=' + symbol
+    headers = {
+        'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+        'accept-encoding' : 'gzip, deflate, br',
+        'accept-language' : 'en-US,en;q=0.9'
+    }
+    response = requests.get(url, headers=headers).content
+    data = json.loads(response.decode('utf-8'))
+    totCE = data['filtered']['CE']['totOI']
+    totPE = data['filtered']['PE']['totOI']
+
+    pcr= totPE/totCE
+    return round(pcr,3)
+
+
 #-----------------------------------------------------------
 #method call
 
-print(pcr_scraper("NIFTY"))
+#print(pcr_stocks_scraper("RELIANCE"))
+
 
 
