3af27ba613d71199dc87a99593b33558e9d75963
Dev V Juneja <78873223+devfinwiz@users.noreply.github.com>
2023-03-21 15:44:19 +0530

    pcr analyzer for stocks added

--- Sentiment/pcr/pcr_analyzer.py
+++ Sentiment/pcr/pcr_analyzer.py
@@ -1,4 +1,4 @@
-from pcr_scraper import pcr_scraper
+from pcr_scraper import pcr_scraper,pcr_stocks_scraper
 
 #-----------------------------------------------------------------------------------------------------------------------------------------
 #Returns a dictionary indicating state of indice (overbought/slightly overbought,oversold,slightly oversold) along with present pcr_value
@@ -9,14 +9,24 @@ def pcr_anal():
     indices=["NIFTY","BANKNIFTY"]
 
     for symbol in indices:
-        pcr_value=pcr_scraper(symbol)
+        try:
+            pcr_value=pcr_scraper(symbol)
+        except:
+            try: 
+                pcr_value=pcr_scraper(symbol)
+            except:
+                try:
+                    pcr_value=pcr_scraper(symbol)
+                except:
+                    pcr_value=pcr_scraper(symbol)
+
         if(pcr_value>=1.4):
             state="Overbought"
             pcr_anal_result[symbol]=[state,pcr_value]
-        elif(pcr_value<1.4 and pcr_value>1):
+        elif(pcr_value<1.4 and pcr_value>=1):
             state="Slightly overbought"
             pcr_anal_result[symbol]=[state,pcr_value]
-        elif(pcr_value<1 and pcr_value>0.6):
+        elif(pcr_value<1 and pcr_value>=0.6):
             state="Slightly oversold"
             pcr_anal_result[symbol]=[state,pcr_value]
         else:
@@ -25,7 +35,43 @@ def pcr_anal():
         
     return pcr_anal_result
 
+#-----------------------------------------------------------------------------------------------------------------------------------------
+#Returns a dictionary indicating state of stock (overbought/slightly overbought,neutral,oversold,slightly oversold) along with present pcr_value
+#Demo Output: {'ADANIENT': ['Neutral', 0.662]}
+
+def pcr_stocks_anal(symbol):
+    try:
+        pcr_anal_result=dict()
+        pcr_value=pcr_stocks_scraper(symbol)
+    except:
+        try:
+            pcr_value=pcr_stocks_scraper(symbol)
+        except:
+            try:
+                pcr_value=pcr_stocks_scraper(symbol)
+            except:
+                pcr_value=pcr_stocks_scraper(symbol)
+
+    if(pcr_value>=1):
+        state="Overbought"
+        pcr_anal_result[symbol]=[state,pcr_value]
+    elif(pcr_value<1 and pcr_value>=0.75):
+        state="Slightly overbought"
+        pcr_anal_result[symbol]=[state,pcr_value]
+    elif(pcr_value<0.75 and pcr_value>=0.50):
+        state="Neutral"
+        pcr_anal_result[symbol]=[state,pcr_value]
+    elif(pcr_value<0.50 and pcr_value>=0.4):
+        state="Slightly oversold"
+        pcr_anal_result[symbol]=[state,pcr_value]
+    else:
+        state="Oversold"
+        pcr_anal_result[symbol]=[state,pcr_value]
+        
+    return pcr_anal_result
+
+
 #--------------------------------------------------------------
 #method call
 
-print(pcr_anal())
+print(pcr_stocks_anal("ADANIENT"))
