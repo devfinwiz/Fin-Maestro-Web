6f5a14376c37ddc3ea88a492dd609ec4f8c32b2d
DEV_FINWIZ <devjuneja43@gmail.com>
2023-05-03 10:03:27 +0530

    minor changes

--- SignalsGenerator/sg_driver.py
+++ SignalsGenerator/sg_driver.py
@@ -16,6 +16,12 @@ with gr.Blocks(title="Fin-Maestro Beta Version",css="#heading{background-color:#
         text_button = gr.Button("Generate Signals",elem_id="generate_sigals")
         plot_output=gr.Plot(label="forecast")    
     
+    with gr.Tab("Instructions"):
+        gr.Label("1. Make sure to add .NS for NSE stocks or .BO for BSE stocks after entering stock symbol in the end.")
+        gr.Label("Valid Example: BSE.NS, VHL.NS, Sahyadri.BO")
+        gr.Label("2. Make sure there is no extra space while entering the stock symbol. In case of any unnecessary extra space, there will be an error. ")
+        gr.Label("3. Consider this as a testing project by a college student. We do not recommend you to take trades based on results from this app.")
+        gr.Label("4. Keep number of signals as 10 (default value) for best results. However, there is a provision for you to modify this value and note the changes.")
     #tut_button.click(signals_generator,inputs=[],outputs=[demo_video])
     text_button.click(signals_generator, inputs=[text_input,no_of_signals], outputs=plot_output)
 
