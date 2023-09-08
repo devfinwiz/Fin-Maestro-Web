1b33ee3cd31b9bfd7a494983df7d9be2a1b045b7
Asaf <46109899+asafravid@users.noreply.github.com>
2023-09-08 16:33:13 +0300

    Update positional_signals.py for latest matplotlib
    Fix contains:
    1. Changes in matplotlib infrastructure of calls
    2. Added Missing import calls

--- SignalsGenerator/positional_signals.py
+++ SignalsGenerator/positional_signals.py
@@ -1,4 +1,7 @@
 import sys
+import matplotlib
+import yfinance as yf
+import matplotlib as plt
 sys.path.append("init")
 from init import *
 
@@ -10,7 +13,7 @@ def signals_generator(ticker,num_of_signals):
     #ticker = input()          #name of the stock for which signals are needed.
     #num_of_signals = 10      #number of buy/sell signals to be plotted
 
-    df = yf.download(ticker,initial,end)    #dataframe from yfinance for specific ticker
+    df = yf.download(ticker)    #dataframe from yfinance for specific ticker
 
     closes = sorted(df.Close.tolist())
     low,high = closes[num_of_signals],closes[-num_of_signals]
@@ -23,15 +26,15 @@ def signals_generator(ticker,num_of_signals):
     long = df.loc[df['Signal'] == 1]
     short = df.loc[df['Signal'] == -1]
 
-    fig=plt.figure()
+    fig=plt.pyplot.figure()
     fig.set_figwidth(12)
-    plt.plot(df.index, df['Adj Close'],color='white', label='Close Price')
-    plt.plot(long.index, df.loc[long.index]['Adj Close'], '^', markersize=10, color='g', label='Long/Buy')
-    plt.plot(short.index, df.loc[short.index]['Adj Close'],'v', markersize=10, color='r', label='Short/Sell')
-    plt.ylabel('Closing Price')
-    plt.xlabel('Date')
-    plt.title(ticker)
-    plt.legend(loc='best')
+    plt.pyplot.plot(df.index, df['Adj Close'],color='white', label='Close Price')
+    plt.pyplot.plot(long.index, df.loc[long.index]['Adj Close'], '^', markersize=10, color='g', label='Long/Buy')
+    plt.pyplot.plot(short.index, df.loc[short.index]['Adj Close'],'v', markersize=10, color='r', label='Short/Sell')
+    plt.pyplot.ylabel('Closing Price')
+    plt.pyplot.xlabel('Date')
+    plt.pyplot.title(ticker)
+    plt.pyplot.legend(loc='best')
 
     #plt.show()
     return fig
